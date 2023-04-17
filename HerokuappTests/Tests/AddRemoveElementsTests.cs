823e6596784dfa6f088c89eecbd98410116a5f7e
kirillparaschenko <kparaschenko@caeonline.com>
2023-04-17 18:48:25 +0300

    Add project files.

---
+++ HerokuappTests/Tests/AddRemoveElementsTests.cs
@@ -0,0 +1,16 @@
+namespace HerokuappTests.Tests
+{
+    public class AddRemoveElementsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
