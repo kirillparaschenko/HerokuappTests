4ddb79d4147add1dd694d564d50afefa526fcee6
kirillparaschenko <kparaschenko@caeonline.com>
2023-04-18 10:57:41 +0300

    Added AddRemoveElementsTests

--- HerokuappTests/Tests/AddRemoveElementsTests.cs
+++ HerokuappTests/Tests/AddRemoveElementsTests.cs
@@ -1,16 +1,37 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace HerokuappTests.Tests
 {
+    [TestFixture]
     public class AddRemoveElementsTests
     {
+        WebDriver ChromeDriver { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/add_remove_elements/");
         }
 
         [Test]
-        public void Test1()
+        public void AddingAndRemovingElements()
         {
-            Assert.Pass();
+            ChromeDriver.FindElement(By.XPath("//button[text()='Add Element']")).Click();
+            ChromeDriver.FindElement(By.XPath("//button[text()='Add Element']")).Click();
+            ChromeDriver.FindElement(By.XPath("//button[text()='Delete']")).Click();
+
+            var elementCounter = ChromeDriver.FindElements(By.XPath("//button[text()='Delete']")).Count;
+            Assert.That(elementCounter, Is.EqualTo(1));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
         }
     }
 }
