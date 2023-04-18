c2b8ee20ed2fd69ed4d4c43b4727fa77bdff4e37
kirillparaschenko <kparaschenko@caeonline.com>
2023-04-18 12:20:23 +0300

    Added VerifyingDropdown test

--- HerokuappTests/Tests/DropdownTests.cs
+++ HerokuappTests/Tests/DropdownTests.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace HerokuappTests.Tests
 {
+    [TestFixture]
     public class DropdownTests
     {
+
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+        }
+
+        [Test]
+        public void VerifyingDropdown()
+        {
+            SelectElement dropDown = new SelectElement(ChromeDriver.FindElement(By.Id("dropdown")));
+
+            dropDown.SelectByText("Option 1");
+            Assert.That(dropDown.SelectedOption.GetAttribute("selected"), Is.EqualTo("true"));
+
+            dropDown.SelectByText("Option 2");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+        }
     }
 }
